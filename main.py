54e2eacb9967a9ce1e874a71d234171f3986129d
Network Based Solutions, LLC <35822984+NBS-LLC@users.noreply.github.com>
2024-04-29 22:59:39 -0700

    feat: use selenium (#2)
    * chore: add selenium dependencies
    
    * chore: enable python auto complete
    
    * poc: load calculator web app
    
    * chore: python defaults are fine
    
    * docs: add usage guide

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://zxcodes.github.io/Calculator")
+print(driver.title)
+
+buttons = driver.find_elements(By.XPATH, '//input[@type="button"]')
+
+for button in buttons:
+    print(f"Clicking: {button.accessible_name}")
+    button.click()
+
+driver.quit()

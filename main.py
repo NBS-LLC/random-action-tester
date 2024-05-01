64c71bf4aefef92afb5149f35d9219f6ba15416d
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-04-30 21:50:21 -0700

    wip: prepare for diff

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
+from textwrap import dedent
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def print_all_elements(driver: webdriver.Remote):
+class Element:
+    def __init__(self, name: str, text: str, value: str):
+        self.name = name
+        self.text = text
+        self.value = value
+
+    def __str__(self) -> str:
+        return dedent(f"""
+            name: {self.name}
+            text: {self.text}
+            value: {self.value}
+        """).strip()
+
+
+def get_all_elements(driver: webdriver.Remote):
+    all_elements = []
     for element in driver.find_elements(By.XPATH, "//*"):
-        print(f"name: {element.accessible_name}")
-        print(f"text: {element.text}")
-        print(f'value: {element.get_attribute("value")}')
+        all_elements.append(
+            Element(
+                element.accessible_name, element.text, element.get_attribute("value")
+            )
+        )
+    return all_elements
+
+
+def print_elements(elements: list[Element]):
+    for element in elements:
+        print(element)
         print()
 
 
@@ -15,7 +40,7 @@ driver.get("http://127.0.0.1:8000/")
 print(f"App Under Test: {driver.title}")
 print()
 
-print_all_elements(driver) # before
+print_elements(get_all_elements(driver))  # before
 
 buttons = driver.find_elements(By.XPATH, '//input[@type="button"]')
 for button in buttons:
@@ -23,7 +48,7 @@ for button in buttons:
     button.click()
 print()
 
-print_all_elements(driver) # after
+print_elements(get_all_elements(driver))  # after
 
 # TODO: diff the before and after, display only the differences
 

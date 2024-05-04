c0f23e70ad3152c33d0f04c2825b1eec0823e51b
Nick Derevjanik <nick@networkbasedsolutions.com>
2024-05-04 00:39:23 -0700

    feat: display xpath of changed elements

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from textwrap import dedent
 from deepdiff import DeepDiff
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class Element:
@@ -18,16 +19,43 @@ class Element:
             value: {self.value}
         """).strip()
 
+    def get_name(self) -> str:
+        return self.name or self.text or self.value or "?"
+
+
+def generate_xpath(child_element: WebElement, current: str):
+    child_tag = child_element.tag_name
+    if child_tag == "html":
+        return "/html[1]" + current
+
+    parent_element = child_element.find_element(By.XPATH, "..")
+    children_elements = parent_element.find_elements(By.XPATH, "*")
+    count = 0
+
+    for _, children_element in enumerate(children_elements):
+        children_tag = children_element.tag_name
+        if child_tag == children_tag:
+            count += 1
+        if child_element == children_element:
+            return generate_xpath(
+                parent_element, "/" + child_tag + "[" + str(count) + "]" + current
+            )
+
+    return None
+
 
 def get_all_elements(driver: webdriver.Remote):
-    all_elements: list[Element] = []
-    for element in driver.find_elements(By.XPATH, "//*"):
-        all_elements.append(
-            Element(
-                element.accessible_name, element.text, element.get_attribute("value")
-            )
+    elements_by_name: dict[str, Element] = {}
+    for web_element in driver.find_elements(By.XPATH, "//*"):
+        element = Element(
+            web_element.accessible_name,
+            web_element.text,
+            web_element.get_attribute("value") or "",
         )
-    return all_elements
+        name = element.get_name()
+        xpath = generate_xpath(web_element, "")
+        elements_by_name[f"({name}){xpath}"] = element
+    return elements_by_name
 
 
 def print_elements(elements: list[Element]):
